d8d7aa45f8d05028ea3071d5f7a061c106ef1807
Srivibhav <sjonnalagad2@wisc.edu>
2024-11-12 21:20:51 -0600

    Added square count logic to cmd_proc.

--- cmd_proc_shell.sv
+++ cmd_proc_shell.sv
@@ -40,19 +40,52 @@ module cmd_proc(clk,rst_n,cmd,cmd_rdy,clr_cmd_rdy,send_resp,strt_cal,
   ///////////////////////////////////
   // Declare any internal signals //
   /////////////////////////////////
-
-
-  //////////////////////////////////////
-  // Count the squares as the Knight //
-  // passes reflectve bands         //
-  ///////////////////////////////////
-  // Implement counter to count number of bits shifted out on the MOSI line.
-  always_ff @(posedge clk) begin
-      bit_cntr <=  (init)  ? 5'h0          : // Reset to 0 initially.
-                   (shift) ? bit_cntr + 1  : // Increment the bit count whenever we shift a bit.
-                   bit_cntr; // Otherwise hold current value.
+  logic move_cmd;            // The command that tells Knight to move from the state machine.
+  logic move_done;           // Indicates that a move is complete by the Knight.
+  logic [3:0] pulse_cnt;     // Indicates number of times cntrIR went high when moving the Knight, max 16 times.
+  logic [2:0] square_cnt;    // The number of squares the Knight moved on the board.
+  logic move_done;           // Indicates that a move is completed by the Knight.
+  logic cntrIR_step;         // Metastable cntrIR signal from the IR sensor.
+	logic cntrIR_stable_prev;  // Stabilized cntrIR signal from the IR sensor.
+  logic cntrIR_stable_curr;  // Used to detect rising edge on the cntrIR signal.
+  //////////////////////////////////////////////////////////////////////////////
+ 
+  ///////////////////////////////////////////////////////////////
+  // Implements square count logic when Knight is told to move /
+  /////////////////////////////////////////////////////////////
+  // Implement rising edge detector to check when cntrIR pulse goes high.
+  always_ff @(posedge clk, negedge rst_n) begin
+    if(!rst_n) begin
+      cntrIR_step <= 1'b0;          // Reset the cntrIR metastable value.
+      cntrIR_stable_prev <= 1'b0;   // Reset the cntrIR stable value.
+      cntrIR_stable_curr <= 1'b0;   // Reset the cntrIR edge detection flop.
+    end else begin
+      cntrIR_step <= cntrIR;                    // Flop the cntrIR signal to correct metastability.
+      cntrIR_stable_prev <= cntrIR_step;        // The synchronized cntrIR signal with the system clock.
+      cntrIR_stable_curr <= cntrIR_stable_prev; // Used to detect rising edge on cntrIR pulse.
+    end
   end
 
+  // A pulse is detected from the cntrIR sensor when the previous value was low and current value is high.
+  assign pulse_detected = ~cntrIR_stable_prev & cntrIR_stable_curr;
+
+  // Implement counter to count number squares moved by the Knight.
+  always_ff @(posedge clk) begin
+    // Load in the number of squares to move when the command is asserted, else hold current value.
+    square_cnt <= (move_cmd) ? cmd[2:0] : square_cnt;  
+  end
+
+  // Implement counter to count number of times the cntrIR pulse went high. 
+  always_ff @(posedge clk) begin
+    pulse_cnt <= (move_cmd)       ? 4'h0           : // Reset to 0 initially when begining a move.
+                 (pulse_detected) ? pulse_cnt + 1  : // Increment the pulse count whenever we detect that cntrIR went high.
+                 pulse_cnt;                          // Otherwise hold current value.
+  end
+
+  // Compare whether the pulse count detected is 2 times the number of sqaures requested to move,
+  // to indicate that a move is complete.
+	assign move_done = (pulse_cnt == {square_cnt, 1'b0});
+  //////////////////////////////////////////////////////////////////////////
 
   ////////////////////////////////////
 	// Implement State Machine Logic //
@@ -66,11 +99,11 @@ module cmd_proc(clk,rst_n,cmd,cmd_rdy,clr_cmd_rdy,send_resp,strt_cal,
         state <= nxt_state; // Store the next state as the current state by default.
   end
 
-  // Implements the combinational state transition and output logic of the state machine.
+  //////////////////////////////////////////////////////////////////////////////////////////
+	// Implements the combinational state transition and output logic of the state machine.//
+	////////////////////////////////////////////////////////////////////////////////////////
 	always_comb begin
-	/////////////////////////////////////////
-  // Default all SM outputs & nxt_state //
-	///////////////////////////////////////
+    /* Default all SM outputs & nxt_state */
     case (state)
       default : begin 
         
