8a05210ef57b46fd4d565f91ee7913f29e0b6b87
Prathmesh-K <44625474+Prathmesh-K@users.noreply.github.com>
2024-11-21 16:37:46 -0600

    added the given functions for TourLogic

--- Knights Tour Logic/TourLogic_shell.sv
+++ Knights Tour Logic/TourLogic_shell.sv
@@ -9,8 +9,6 @@ module TourLogic(clk,rst_n,x_start,y_start,go,done,indx,move);
   //signal to zero the board
   logic zero;
   //signal to initialize the board
-  logic go;
-  //signal to indicate if the next position is visited already
   logic not_visited;
   //signal to indicate if the next move is possible
   logic possible;
@@ -30,6 +28,7 @@ module TourLogic(clk,rst_n,x_start,y_start,go,done,indx,move);
   reg [7:0] move_try;				// one hot encoding of move we will try next
   reg [4:0] move_num;				// keeps track of move we are on
   reg [2:0] xx,yy;					// current x & y position  
+  logic [2:0] nxt_xx,nxt_yy;			// next x & y position
 
   //Creating the states for the state machine
   typedef enum logic [2:0] {
@@ -39,9 +38,9 @@ module TourLogic(clk,rst_n,x_start,y_start,go,done,indx,move);
     MAKE_MOVE,
     BACKUP
   } state_t;
+
+  state_t state, next_state;
   
-
-
   //We need a counter to keep track of order of moves to track where on the board the knight has visited
   << 2-D array of 5-bit vectors that keep track of where on the board the knight
      has visited.  Will be reduced to 1-bit boolean after debug phase >>
@@ -76,6 +75,8 @@ module TourLogic(clk,rst_n,x_start,y_start,go,done,indx,move);
   
   << Your magic occurs here >>
   
+
+  //We can create a set reset flop for the move count to prevent race conditions 
   
   
   function [7:0] calc_poss(input [2:0] xpos,ypos);
@@ -84,6 +85,30 @@ module TourLogic(clk,rst_n,x_start,y_start,go,done,indx,move);
 	// all the possible moves (at least in bound) moves given
 	// coordinates of Knight.
 	/////////////////////////////////////////////////////
+    //initialize the possible moves to 0
+    logic [7:0] poss_moves;
+    poss_moves = 8'b0;
+    // $xoff{1} = 1; $yoff{1} = 2;
+    // $xoff{2} = -1; $yoff{2} = 2;
+    // $xoff{4} = -2; $yoff{4} = 1;
+    // $xoff{8} = -2; $yoff{8} = -1;
+    // $xoff{16} = -1; $yoff{16} = -2;
+    // $xoff{32} = 1; $yoff{32} = -2;
+    // $xoff{64} = 2; $yoff{64} = -1;
+    // $xoff{128} = 2; $yoff{128} = 1;
+    
+    logic signed [2:0] x_offsets[8] = '{1, -1, -2, -2, -1, 1, 2, 2};
+    logic signed [2:0] y_offsets[8] = '{2, 2, 1, -1, -2, -2, -1, 1};
+    poss_moves = 8'b0;
+    
+    for (int i = 0; i < 8; i++) begin
+      logic signed [3:0] newx = xpos + x_offsets[i];
+      logic signed [3:0] newy = ypos + y_offsets[i];
+      if (newx >= 0 && newx < 5 && newy >= 0 && newy < 5)
+        poss_moves[i] = 1;
+    end
+    return poss_moves;
+  
   endfunction
   
   function signed [2:0] off_x(input [7:0] try);
@@ -94,6 +119,17 @@ module TourLogic(clk,rst_n,x_start,y_start,go,done,indx,move);
 	// by passing in last move you did try, and subtracting 
 	// the resulting offset from xx
 	/////////////////////////////////////////////////////
+  case (try)
+    8'b0000_0001: return 1;
+    8'b0000_0010: return -1;
+    8'b0000_0100: return -2;
+    8'b0000_1000: return -2;
+    8'b0001_0000: return -1;
+    8'b0010_0000: return 1;
+    8'b0100_0000: return 2;
+    8'b1000_0000: return 2;
+    default: return 0;
+  endcase
   endfunction
   
   function signed [2:0] off_y(input [7:0] try);
@@ -104,6 +140,17 @@ module TourLogic(clk,rst_n,x_start,y_start,go,done,indx,move);
 	// by passing in last move you did try, and subtracting 
 	// the resulting offset from yy
 	/////////////////////////////////////////////////////
+  case (try)
+    8'b0000_0001: return 2;
+    8'b0000_0010: return 2;
+    8'b0000_0100: return 1;
+    8'b0000_1000: return -1;
+    8'b0001_0000: return -2;
+    8'b0010_0000: return -2;
+    8'b0100_0000: return -1;
+    8'b1000_0000: return 1;
+    default: return 0;
+  endcase
   endfunction
   
 endmodule
