643f5ca4237acbe027d1999262ddf62f7a1d769d
Srivibhav <sjonnalagad2@wisc.edu>
2024-12-05 06:54:21 -0600

    Made change to tb.

--- KnightsTour/tests/simple/KnightsTour_tb_1.sv
+++ KnightsTour/tests/simple/KnightsTour_tb_1.sv
@@ -5,89 +5,48 @@ module KnightsTour_tb();
   localparam FAST_SIM = 1;
   
   ///////////////////////////
-// Stimulus of type reg //
-///////////////////////////
-reg clk, RST_n;
-reg [15:0] cmd;
-reg send_cmd;
+  // Stimulus of type reg //
+  /////////////////////////
+  reg clk, RST_n;
+  reg [15:0] cmd;
+  reg send_cmd;
 
-/////////////////////////////////
-// Declare any internal signals //
-///////////////////////////////
-wire SS_n, SCLK, MOSI, MISO, INT;
-wire lftPWM1, lftPWM2, rghtPWM1, rghtPWM2;
-wire TX_RX, RX_TX;
-logic cmd_sent;
-logic resp_rdy;
-logic [7:0] resp;
-wire IR_en;
-wire lftIR_n, rghtIR_n, cntrIR_n;
-
-// Missing signals
-wire piezo, piezo_n;  // Declare piezo signals for buzzer
-wire cal_done;        // To check if calibration is done
-
-//////////////////////
-// Instantiate DUT //
-////////////////////
-KnightsTour #(FAST_SIM) iDUT(
-    .clk(clk),
-    .RST_n(RST_n),
-    .SS_n(SS_n),
-    .SCLK(SCLK),
-    .MOSI(MOSI),
-    .MISO(MISO),
-    .INT(INT),
-    .lftPWM1(lftPWM1),
-    .lftPWM2(lftPWM2),
-    .rghtPWM1(rghtPWM1),
-    .rghtPWM2(rghtPWM2),
-    .RX(TX_RX),
-    .TX(RX_TX),
-    .piezo(piezo),        // Connected to piezo signal
-    .piezo_n(piezo_n),    // Connected to piezo_n signal
-    .IR_en(IR_en),
-    .lftIR_n(lftIR_n),
-    .rghtIR_n(rghtIR_n),
-    .cntrIR_n(cntrIR_n),
-    .cal_done(cal_done)   // Calibration done signal
-);
-
-///////////////////////////////////////////////////
-// Instantiate RemoteComm to send commands to DUT //
-///////////////////////////////////////////////////
-RemoteComm iRMT(
-    .clk(clk),
-    .rst_n(RST_n),
-    .RX(RX_TX),
-    .TX(TX_RX),
-    .cmd(cmd),
-    .send_cmd(send_cmd),
-    .cmd_sent(cmd_sent),
-    .resp_rdy(resp_rdy),
-    .resp(resp)
-);
-
-////////////////////////////////////////////////////
-// Instantiate model of Knight Physics (and board) //
-////////////////////////////////////////////////////
-KnightPhysics iPHYS(
-    .clk(clk),
-    .RST_n(RST_n),
-    .SS_n(SS_n),
-    .SCLK(SCLK),
-    .MISO(MISO),
-    .MOSI(MOSI),
-    .INT(INT),
-    .lftPWM1(lftPWM1),
-    .lftPWM2(lftPWM2),
-    .rghtPWM1(rghtPWM1),
-    .rghtPWM2(rghtPWM2),
-    .IR_en(IR_en),
-    .lftIR_n(lftIR_n),
-    .rghtIR_n(rghtIR_n),
-    .cntrIR_n(cntrIR_n)
-);
+  ///////////////////////////////////
+  // Declare any internal signals //
+  /////////////////////////////////
+  wire SS_n,SCLK,MOSI,MISO,INT;
+  wire lftPWM1,lftPWM2,rghtPWM1,rghtPWM2;
+  wire TX_RX, RX_TX;
+  logic cmd_sent;
+  logic resp_rdy;
+  logic [7:0] resp;
+  wire IR_en;
+  wire lftIR_n,rghtIR_n,cntrIR_n;
+  wire piezo, piezo_n;
+  
+  //////////////////////
+  // Instantiate DUT //
+  ////////////////////
+  KnightsTour #(FAST_SIM) iDUT(.clk(clk), .RST_n(RST_n), .SS_n(SS_n), .SCLK(SCLK),
+                   .MOSI(MOSI), .MISO(MISO), .INT(INT), .lftPWM1(lftPWM1),
+				   .lftPWM2(lftPWM2), .rghtPWM1(rghtPWM1), .rghtPWM2(rghtPWM2),
+				   .RX(TX_RX), .TX(RX_TX), .piezo(piezo), .piezo_n(piezo_n),
+				   .IR_en(IR_en), .lftIR_n(lftIR_n), .rghtIR_n(rghtIR_n),
+				   .cntrIR_n(cntrIR_n));
+				  
+  /////////////////////////////////////////////////////
+  // Instantiate RemoteComm to send commands to DUT //
+  ///////////////////////////////////////////////////
+  RemoteComm iRMT(.clk(clk), .rst_n(RST_n), .RX(RX_TX), .TX(TX_RX), .cmd(cmd),
+             .snd_cmd(send_cmd), .cmd_sent(cmd_sent), .resp_rdy(resp_rdy), .resp(resp));
+				   
+  //////////////////////////////////////////////////////
+  // Instantiate model of Knight Physics (and board) //
+  ////////////////////////////////////////////////////
+  KnightPhysics iPHYS(.clk(clk),.RST_n(RST_n),.SS_n(SS_n),.SCLK(SCLK),.MISO(MISO),
+                      .MOSI(MOSI),.INT(INT),.lftPWM1(lftPWM1),.lftPWM2(lftPWM2),
+					  .rghtPWM1(rghtPWM1),.rghtPWM2(rghtPWM2),.IR_en(IR_en),
+					  .lftIR_n(lftIR_n),.rghtIR_n(rghtIR_n),.cntrIR_n(cntrIR_n)); 
 	
   ///////////////////////////////////////////////////////////
   // Test procedure to apply stimulus and check responses //
