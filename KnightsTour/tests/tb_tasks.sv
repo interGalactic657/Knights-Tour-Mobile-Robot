6cca4cd4a171c7ea389d684a7fa116e875695097
Srivibhav <sjonnalagad2@wisc.edu>
2024-12-05 19:54:56 -0600

    modifed tb.

--- KnightsTour/tests/tb_tasks.sv
+++ KnightsTour/tests/tb_tasks.sv
@@ -158,13 +158,13 @@ package tb_tasks;
     begin
       fork
         begin : wait_moving
-          repeat(6000000) @(negedge clk);
+          repeat(600000000) @(negedge clk);
           // Never crossed threshold
           $display("ERROR: velocity sum is not crossing 0x1000 threshold\nvelocity sum: 0x%h", velocity_sum);
           $stop();
         end
         begin : check_moving
-          repeat(6000000) @(negedge clk) begin
+          repeat(600000000) @(negedge clk) begin
             if (velocity_sum >= $signed(17'h01000)) begin
               disable wait_moving;
               disable check_moving;
