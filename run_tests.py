90277cd14dc037231f3ffbb76c79d139c8bb0f6c
Srivibhav <sjonnalagad2@wisc.edu>
2024-12-07 16:33:40 -0600

    Modified script.

--- run_tests.py
+++ run_tests.py
@@ -128,7 +128,7 @@ def run_testbench(subdir, test_file, mode, debug_mode):
         os.chdir(post_synthesis_dir)
 
         # Run post-synthesis specific steps with expanded paths
-        sim_command = f"vsim -c -do 'project open {os.path.expanduser('~/PostSynthesis.mpf')}; " \
+        sim_command += f"vsim -c -do 'project open {os.path.expanduser('~/PostSynthesis.mpf')}; " \
                       f"project compileall; vsim work.KnightsTour_tb -t ns -L {os.path.expanduser('~/ece551/SAED32_lib')} " \
                       f"-Lf {os.path.expanduser('~/ece551/SAED32_lib')} -voptargs=+acc'"
     else:
