8d943e279bcbf246df3a5060461c4e95ed00401d
Srivibhav <sjonnalagad2@wisc.edu>
2024-12-07 15:38:15 -0600

    Modified script.

--- run_tests.py
+++ run_tests.py
@@ -132,7 +132,7 @@ def run_testbench(subdir, test_file, mode, debug_mode):
         sim_command.extend([
             "vsim",
             "-c",
-            f"\"project open {os.path.join(post_synthesis_dir, 'PostSynthesis.mpf')}; project compileall;\""
+            f"\"project open {os.path.join(post_synthesis_dir, 'PostSynthesis.mpf')}; project compileall; quit\""
         ])
     else:
         subprocess.run(f"vlog +acc {test_path}", shell=True, check=True)
