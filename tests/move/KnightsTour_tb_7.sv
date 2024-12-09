a00547c334b3537d394da88ff5ab011dab607209
Srivibhav <sjonnalagad2@wisc.edu>
2024-12-08 22:56:31 -0600

    Added off the board check task.

--- tests/move/KnightsTour_tb_7.sv
+++ tests/move/KnightsTour_tb_7.sv
@@ -103,7 +103,7 @@ module KnightsTour_tb();
 
   // Checks that we are never off the board.
   always @(negedge clk)
-    ChkOffBoard(.clk(clk), .RST_n(RST_n), .state(iDUT.iCMD.state), .frwrd(iDUT.iCMD.frwrd), .cntrIR(iDUT.iCMD.cntrIR));
+    ChkOffBoard(.clk(clk), .RST_n(RST_n), .frwrd(iDUT.iCMD.frwrd), .cntrIR(iDUT.cntrIR));
   
   always
     #5 clk = ~clk;
